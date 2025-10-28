0be4e2a2a05511e520a31572d483f25bf22ff5fe
Elizaveta Shestakova <e.shestakova@megaplan.ru>
2025-10-28 16:26:11 +0300

    Переделала главную

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import "./index.css"
+import "@radix-ui/themes/styles.css"
+import { Theme, ThemePanel } from "@radix-ui/themes"
 import App from "./App.tsx"
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <App />
+        <Theme accentColor="cyan" radius="large">
+            <App />
+            {/* <ThemePanel /> */}
+        </Theme>
     </StrictMode>
 )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,29 +1,29 @@
+import { Flex, Text, Button, Heading, Box } from "@radix-ui/themes"
+import { MdPets } from "react-icons/md"
+import { FaGamepad } from "react-icons/fa"
+import { SiAccuweather } from "react-icons/si"
 import { Link } from "react-router-dom"
 
 export default function Home() {
     const projects = [
-        { name: "2048 Game", path: "/game2048" },
-        { name: "Weather App", path: "/weatherApp" }
+        { name: "2048 Game", path: "/game2048", iconComponent: <FaGamepad /> },
+        { name: "Weather App", path: "/weatherApp", iconComponent: <SiAccuweather /> }
     ]
 
     return (
-        <div className="min-h-screen bg-gradient-to-br from-amber-100 to-amber-200 dark:from-slate-800 dark:to-slate-900 text-center p-6">
-            <h1 className="text-4xl font-bold mb-8 text-amber-700 dark:text-amber-300">🌟 My Pet Projects</h1>
-
-            <div className="grid gap-6 max-w-md mx-auto">
-                {projects.map((p) => (
-                    <Link
-                        key={p.path}
-                        to={p.path}
-                        className="block py-4 rounded-2xl font-semibold text-lg
-              bg-gradient-to-br from-white to-amber-100 dark:from-slate-700 dark:to-slate-800
-              shadow-[4px_4px_8px_rgba(0,0,0,0.25),-3px_-3px_6px_rgba(255,255,255,0.7)]
-              hover:scale-105 active:scale-95 transition-transform duration-150"
-                    >
+        <Flex direction="column" gap="2" mt="4">
+            <Flex justify="center" gap="2">
+                <MdPets size="24" color="#0090FF" />
+                <Heading mb="2">My Pet Projects</Heading>
+            </Flex>
+            {projects.map((p) => (
+                <Link key={p.path} to={p.path}>
+                    <Button size="3">
+                        {p.iconComponent}
                         {p.name}
-                    </Link>
-                ))}
-            </div>
-        </div>
+                    </Button>
+                </Link>
+            ))}
+        </Flex>
     )
 }
